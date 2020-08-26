2d371d5f70dbf060062faf84e03ff267617c211b
Adam Elek <elekadam21@gmail.com>
2020-08-26 13:31:05 +0200

    created the basics for the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  state = {
+    pokemons: [
+      {
+        id: 1,
+        title: "Pikachu",
+      },
+      {
+        id: 2,
+        title: "Bulbasaur",
+      },
+      {
+        id: 1,
+        title: "Squirtle",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Reactomon</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
