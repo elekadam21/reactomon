036752140b1e6dc7122736545519dbc9d029f22d
Adam Elek <elekadam21@gmail.com>
2020-08-29 16:18:20 +0200

    App.js changed from class to function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import PokemonList from "./components/PokemonList";
@@ -8,22 +8,20 @@ import Header from "./components/Header";
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <React.Fragment>
-          <div className="App">
-            <Header />
-            <Navbar />
-            <Route path="/pokemons" render={(props) => <PokemonList />} />
-            <Route path="/types" render={(props) => <TypeList />} />
-            <Route path="/pokemon/:id" children={<PokemonDetail />} />
-          </div>
-        </React.Fragment>
-      </Router>
-    );
-  }
-}
+const App = (props) => {
+  return (
+    <Router>
+      <React.Fragment>
+        <div className="App">
+          <Header />
+          <Navbar />
+          <Route path="/pokemons" render={(props) => <PokemonList />} />
+          <Route path="/types" render={(props) => <TypeList />} />
+          <Route path="/pokemon/:id" children={<PokemonDetail />} />
+        </div>
+      </React.Fragment>
+    </Router>
+  );
+};
 
 export default App;
